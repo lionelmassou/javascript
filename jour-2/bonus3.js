bd67d2c0d9291cf06fbd7a27c3ec3839a4a03d56
lionel massou <massou.lionel@gmail.com>
2021-03-09 18:01:35 +0100

    Exercices jour 2 FINI

--- jour-2/bonus3.js
+++ jour-2/bonus3.js
@@ -6,7 +6,7 @@ var roundedNumber = 3.6;
 
 console.log("valeur à obtenir TEST1: " + Math.round(roundedNumber));
 
-if (Math.ceil(roundedNumber)-roundedNumber < 0.5) {
+if (Math.ceil(roundedNumber)-roundedNumber <= 0.5) {
     console.log("résultat1: " + Math.ceil(roundedNumber));
 } else {
     console.log("résultat1: " + Math.floor(roundedNumber));
@@ -17,7 +17,7 @@ var roundedNumber = 3.3;
 
 console.log("valeur à obtenir TEST2: " + Math.round(roundedNumber));
 
-if (Math.ceil(roundedNumber)-roundedNumber < 0.5 && (Math.floor(roundedNumber))-roundedNumber <= 0) {
+if (Math.ceil(roundedNumber)-roundedNumber <= 0.5) {
     console.log("résultat2: " + Math.ceil(roundedNumber));
 } else {
     console.log("résultat2: " + Math.floor(roundedNumber));
@@ -28,7 +28,7 @@ var roundedNumber = 3.8;
 
 console.log("valeur à obtenir TEST3: " + Math.round(roundedNumber));
 
-if (Math.ceil(roundedNumber)-roundedNumber < 0.5 && (Math.floor(roundedNumber))-roundedNumber <= 0) {
+if (Math.ceil(roundedNumber)-roundedNumber <= 0.5) {
     console.log("résultat3: " + Math.ceil(roundedNumber));
 } else {
     console.log("résultat3: " + Math.floor(roundedNumber));
@@ -39,7 +39,7 @@ var roundedNumber = 12.4;
 
 console.log("valeur à obtenir TEST4: " + Math.round(roundedNumber));
 
-if (Math.ceil(roundedNumber)-roundedNumber < 0.5 && (Math.floor(roundedNumber))-roundedNumber <= 0) {
+if (Math.ceil(roundedNumber)-roundedNumber <= 0.5) {
     console.log("résultat4: " + Math.ceil(roundedNumber));
 } else {
     console.log("résultat4: " + Math.floor(roundedNumber));
@@ -47,13 +47,20 @@ if (Math.ceil(roundedNumber)-roundedNumber < 0.5 && (Math.floor(roundedNumber))-
 
 // METHODE CHAINE DE CHARACTER
 
-var roundedNumber = 3.3;
-console.log("valeur à obtenir TEST1: " + Math.round(roundedNumber));
+var roundedNumber = 99.9;
+// console.log("valeur à obtenir TEST1: " + Math.round(roundedNumber));
 
 var roundedString = roundedNumber.toString()
 
-// console.log(roundedString.charAt(2)+1);
+var searchTerm = '.';
+var indexOfFirst = roundedString.indexOf(searchTerm);
+// console.log(indexOfFirst);
 
-if (roundedString.charAt(2)>="0.5") {
-    console.log("résultat1bis: " + (roundedString.charAt(0)+1));
+if (roundedString.charAt(indexOfFirst+1)<"5") {
+    console.log("résultat1bis: " + (roundedString.substring(0, indexOfFirst)));
+} else {
+    roundedNumber = roundedNumber + 1;
+    roundedString = roundedNumber.toString()
+    indexOfSecond = roundedString.indexOf(searchTerm)
+    console.log("résultat1bis: " + (roundedString.substring(0, indexOfSecond)));
 }
