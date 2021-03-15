f3a45e4dc77aa181e560a9bf2e64f09258a75ff1
lionel massou <massou.lionel@gmail.com>
2021-03-15 15:56:07 +0100

    Revisions TMP

--- revisions/exo2.js
+++ revisions/exo2.js
@@ -6,17 +6,33 @@ function countEach(str) {
     var valO = 0;
 
     for (var i = 0; i < str.length; i++) {
-        console.log(str.charAt[i]);
-        
-        if ("x" === str.charAt[i]){
-            
+        // console.log(str.charAt[i]);
+
+        if (str.charAt(i) === "x") {
+
             valX = valX + 1;
+            console.log("valX = ", valX);
         } else {
             valO = valO + 1;
+            console.log("valO = ", valO);
+
         }
     }
+
     console.log(valX);
     console.log(valO);
+
+
+    if (valX === valO) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
 }
 
-countEach("xoox");
+// countEach("xoox");
+// countEach("xox");
+countEach("xoooxx");
+
+
