4b73c08b1c87668283062117e98343667dfc6dd1
lionel massou <massou.lionel@gmail.com>
2021-03-17 12:30:35 +0100

    Exercices Jour 5 TMP

--- jour-5/exercice1.js
+++ jour-5/exercice1.js
@@ -1,13 +1,26 @@
 // CALCULATOR
 
-function calculate (num1, num2, parm) {
-    if (parm === "+") return num1+num2;
-    if (parm === "*") return num1*num2;
-    if (parm === "/") return num1/num2;
-    if (parm === "-") return num1-num2;
+function calculate(num1, num2, parm) {
+    if (parm === "+") return num1 + num2;
+    else if (parm === "*") return num1 * num2;
+    else if (parm === "/") return num1 / num2;
+    else if (parm === "-") return num1 - num2;
+    else if (parm === "%") return num1 % num2;
+    else console.log("error");
 }
 
-console.log("la somme est: ", calculate(2,3, "+"));
-console.log("le produit est: ", calculate(5,4, "*"));
-console.log("la division est: ", calculate(2,3, "/"));
-console.log("la soustraction est: ", calculate(2,3, "-"));
+firstnum1 = parseInt(process.argv[2])
+secondnum2 = parseInt(process.argv[3])
+thirdparm = process.argv[4]
+
+// if (thirdparm == "+") console.log(calculate(firstnum1 + secondnum2));
+// if (thirdparm == "-") console.log(calculate(firstnum1 - secondnum2));
+// if (thirdparm == "*") console.log(calculate(firstnum1 * secondnum2));
+// if (thirdparm == "/") console.log(calculate(firstnum1 / secondnum2));
+
+// console.log("la somme est: ", calculate(2,3, "+"));
+// console.log("le produit est: ", calculate(5,4, "*"));
+// console.log("la division est: ", calculate(2,3, "/"));
+// console.log("la soustraction est: ", calculate(2,3, "-"));
+
+console.log(calculate(firstnum1, secondnum2, thirdparm));
