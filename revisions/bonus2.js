e13ea928e171281fb8d738f5cd601325f64d61a1
Massou <massou.lionel@gmail.com>
2021-03-19 17:33:39 +0100

    Exercice jour-6 TMP

--- revisions/bonus2.js
+++ revisions/bonus2.js
@@ -1,12 +1,14 @@
-// ALPHABET
+// BONUS 2 alphabet
 
 function sortLetters (str) {
     var tab = str.split("");
-    console.log(tab);
-    tab.sort();
+    // console.log(tab);
+    // tab.sort();
+
+    tab.sort((a, b) => a.localeCompare(b, 'fr', {ignorePunctuation: true}));
     
     return tab.join("");
     // console.log(tab);
 }
 
-console.log(sortLetters("konexio")); // retourne "eiknoox"
+console.log(sortLetters("KoNexio")); // retourne "eiknoox"
---
+++ revisions/bonus3.js
@@ -0,0 +1,38 @@
+// // BONUS 3 xoxo
+
+// function countEach(str) {
+
+//     var valX = 0;
+//     var valO = 0;
+
+//     for (var i = 0; i < str.length; i++) {
+//         // console.log(str.charAt[i]);
+
+//         if (str.charAt(i) === "x") {
+
+//             valX = valX + 1;
+//             console.log("valX = ", valX);
+//         } else {
+//             valO = valO + 1;
+//             console.log("valO = ", valO);
+
+//         }
+//     }
+
+//     console.log(valX);
+//     console.log(valO);
+
+
+//     if (valX === valO) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+
+// }
+
+// // countEach("xoox");
+// countEach("xox");
+// // countEach("xoooxx");
+
+
